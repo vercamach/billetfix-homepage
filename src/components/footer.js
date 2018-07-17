50c1a3f230d00bec8b206c63bf057325790210cc
vercamach <machackovaver@gmail.com>
2018-07-17 15:10:33 +0200

    .

---
+++ src/components/footer.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+import FBIcon from '../img/fb-icon.png'
+
+const FooterSection = styled.footer`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  text-align: center;
+  background-color: #f7f7f7;
+  width: 100%;
+
+  > ul {
+    list-style-type: none;
+    text-align: left;
+    margin: 0;
+    padding: 25px;
+    min-width: 300px;
+  }
+  > ul:nth-child(2) li {
+    font-weight: 700;
+    font-size: 14px;
+    text-align: center;
+    line-height: 1.8em;
+    letter-spacing: 1px;
+  }
+  > ul.right-column {
+    text-align: right;
+  }
+  li,
+  a.ticketbutler-link,
+  span {
+    font-weight: 100;
+    line-height: 20px;
+    font-size: 13px;
+    letter-spacing: 1px;
+  }
+  a {
+    color: #ff4a6e;
+    text-decoration: none;
+  }
+  img {
+    height: 24px;
+    width: 24px;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 5px;
+  }
+`
+
+const Footer = ({}) => (
+  <div>
+    <FooterSection>
+      <ul className="left-column">
+        <li> Ticketing delivered by:</li>
+        <a
+          className="ticketbutler-link"
+          href="https://ticketbutler.io"
+          target="_blank"
+        >
+          Ticketbutler
+        </a>
+        <span> (CVR: 38404687)</span>
+      </ul>
+      <ul className="middle-column">
+        <li>
+          <Link to="//">Home</Link>
+        </li>
+        <li>
+          <Link to="//">Contact</Link>
+        </li>
+        <li>
+          <Link to="//">Privacy Policy</Link>
+        </li>
+        <li>
+          <Link to="//">Terms & Conditions</Link>
+        </li>
+        <li>
+          <img src={FBIcon} alt="fb-icon" />
+          <a href="https://www.facebook.com/BilletFix/"> Find us on Facebook</a>
+        </li>
+      </ul>
+      <ul className="right-column">
+        <li>BilletFix IVS</li>
+        <li>CVR: 37073911</li>
+        <li>Porcelænshaven 26 1.</li>
+        <li>Frederiksberg 2000</li>
+        <li>89 80 12 80</li>
+        <li>
+          <a href="mailto:info@billetfix.dk">info@billetfix.dk</a>
+        </li>
+      </ul>
+    </FooterSection>
+  </div>
+)
+
+export default Footer
---
+++ src/components/header.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Logo from '../img/logox400.png'
+import '../layouts/styles/layout-overide.css'
+
+let headerStyles = {
+  nav: {
+    padding: '15px 5%',
+    top: '0',
+    width: '100%',
+    display: 'flex',
+    position: 'fixed',
+    justifyContent: 'space-between',
+    zIndex: '999',
+    transition: 'all 0.5s',
+  },
+  createEvent: {
+    margin: '-11px auto',
+    padding: '8px 13px',
+    background: '#ff4a6e',
+    color: '#fff',
+    letterSpacing: '2px',
+    borderRadius: '2px',
+    cursor: 'pointer',
+    fontSize: '13px',
+    textAlign: 'center',
+    fontWeight: '400',
+    border: '1px solid #ff4a6e',
+    transition: 'all .15s',
+  },
+  linkDiv: {
+    display: 'block',
+    margin: '10px',
+    padding: '0',
+  },
+  link: {
+    marginLeft: '16px',
+    textDecoration: 'none',
+    display: 'inline-block',
+    padding: '0 10px',
+    fontSize: '13px',
+    color: '#f7f7f7',
+    transition: 'all 0.5s',
+  },
+}
+
+const Header = ({ siteTitle }) => (
+  <div>
+    <nav id="nav" style={headerStyles.nav}>
+      <Link to="/index-page/">
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{
+            margin: '0',
+            maxWidth: '160px',
+            maxHeight: '70px',
+          }}
+        />
+      </Link>
+
+      <div id="navDiv" style={headerStyles.linkDiv}>
+        <Link to="/create-event">
+          <button
+            style={headerStyles.createEvent}
+            className="createEventButton"
+          >
+            create event
+          </button>
+        </Link>
+        <Link style={headerStyles.link} to="/about/">
+          About
+        </Link>
+        <Link style={headerStyles.link} to="/faq/">
+          {' '}
+          FAQ{' '}
+        </Link>
+        <Link
+          className="link"
+          style={headerStyles.link}
+          to="/enterprise-solution/"
+        >
+          {' '}
+          Enterprise solution{' '}
+        </Link>
+        <Link className="link" style={headerStyles.link} to="/my-tickets/">
+          {' '}
+          My tickets{' '}
+        </Link>
+        <Link id="link" style={headerStyles.link} to="/login/">
+          Login
+        </Link>
+      </div>
+    </nav>
+  </div>
+)
+{
+  window.onscroll = () => {
+    const nav = document.getElementById('nav')
+
+    if (scrollY <= 10) (nav.className = ''), (link.className = '')
+    else (nav.className = 'scroll-header'), (link.className = 'scroll-link')
+  }
+}
+export default Header
