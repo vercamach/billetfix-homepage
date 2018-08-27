6f0fbacf780b30929c9bbb32707de17fa842f0de
vercamach <machackovaver@gmail.com>
2018-08-27 12:08:57 +0200

    d

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,252 +3,256 @@ import { Layout } from '../layouts/index'
 import { WindowHelper } from '../components/helpers'
 import 'font-awesome/css/font-awesome.min.css'
 
-let style = {
-  icons: {
-    display: 'inline',
-    float: 'left',
-    fontSize: '20px',
-  },
-  contactTitles: {
-    display: 'block',
-    textAlign: 'center',
-    width: '250px',
-    textAlign: 'left',
-    fontSize: '25px',
-    fontWeight: '400',
-    marginBottom: '10px',
-    ...(window.width > 980
-      ? { marginLeft: '50px' }
-      : { marginLeft: '30px', fontSize: '20px' }),
-  },
-  contactText: {
-    letterSpacing: '1px',
-    display: 'inherit',
-    textAlign: 'left',
-    marginLeft: '30px',
-    marginTop: '0px',
-    marginBottom: '0px',
-    width: '250px',
-    fontWeight: '300',
-    ...(window.width > 980 ? { fontSize: '18px' } : { fontSize: '16px' }),
-  },
-}
-
 const Contact = () => (
   <Layout>
     <WindowHelper>
-      {window => (
-        <section
-          style={{
-            clear: 'both',
+      {window => {
+        let style = {
+          icons: {
+            display: 'inline',
+            float: 'left',
+            fontSize: '20px',
+          },
+          contactTitles: {
+            display: 'block',
             textAlign: 'center',
-            padding: '100px 0',
-            background: '#fff',
-            color: '#333',
-          }}
-        >
-          <div
+            width: '250px',
+            textAlign: 'left',
+            fontSize: '25px',
+            fontWeight: '400',
+            marginBottom: '10px',
+            ...(window.width > 980
+              ? { marginLeft: '50px' }
+              : { marginLeft: '30px', fontSize: '20px' }),
+          },
+          contactText: {
+            letterSpacing: '1px',
+            display: 'inherit',
+            textAlign: 'left',
+            marginLeft: '30px',
+            marginTop: '0px',
+            marginBottom: '0px',
+            width: '250px',
+            fontWeight: '300',
+            ...(window.width > 980
+              ? { fontSize: '18px' }
+              : { fontSize: '16px' }),
+          },
+        }
+
+        return (
+          <section
             style={{
-              ...(window.width > 980
-                ? {
-                    width: '50%',
-                    float: 'left',
-                  }
-                : {
-                    display: 'block',
-                    width: '100%',
-                    paddingBottom: '70px',
-                  }),
+              clear: 'both',
+              textAlign: 'center',
+              padding: '100px 0',
+              background: '#fff',
+              color: '#333',
             }}
           >
-            <form>
-              <h1
-                style={{
-                  fontWeight: '200',
-
-                  lineHeight: '1.5em',
-                  letterSpacing: '3px',
-                  ...(window.width > 980
-                    ? {
-                        fontSize: '50px',
-                      }
-                    : {
-                        fontSize: '28px',
-                      }),
-                }}
-              >
-                Contact
-              </h1>
-              <label
-                for="send-address"
-                style={{
-                  textAlign: 'center',
-                  display: 'block',
-                  marginTop: '25px',
-                  fontWeight: '100',
-                  color: '#333',
-                }}
-              >
-                Your email please
-              </label>
-              <input
-                required
-                id="send-address"
-                maxLength="256"
-                name="send address"
-                type="email"
-                style={{
-                  display: 'block',
-                  margin: '10px auto',
-                  maxWidth: '300px',
-                  width: '300px',
-                  border: '1px solid rgba(255,74,110,.5)',
-                  borderRadius: '3px',
-                  padding: '10px 15px',
-                  outline: '#000',
-                  fontSize: 'inherit',
-                }}
-              />
-              <label
-                for="message"
-                style={{
-                  fontWeight: '100',
-                  textAlign: 'center',
-                  display: 'block',
-                  marginTop: '25px',
-                  color: '#333',
-                }}
-              >
-                Message
-              </label>
-              <textarea
-                required
-                id="message"
-                rows="10"
-                style={{
-                  outline: 'none',
-                  width: '90%',
-                  maxWidth: ' 400px',
-                  fontFamily: 'inherit',
-                  fontSize: 'inherit',
-                  margin: '10px',
-                  lineHeight: 'inherit',
-                  border: '1px solid rgba(255,74,110,.5)',
-                  borderRadius: '3px',
-                  padding: '.5em .75em',
-                }}
-              />
-              <button
-                type="submit"
-                style={{
-                  letterSpacing: '2px',
-                  borderColor: '#ff4a6e',
-                  display: 'block',
-                  background: '#ff4a6e',
-                  color: '#fff',
-                  borderRadius: '2px',
-                  margin: ' auto',
-                  cursor: 'pointer',
-                  padding: '10px 25px',
-                  textAlign: 'center',
-                  fontWeight: '300',
-                  fontSize: '13px',
-                  border: '1px solid #ff4a6e',
-
-                  transition: 'all .15s',
-                }}
-              >
-                SEND!
-              </button>
-            </form>
-          </div>
-          <div
-            style={{
-              backgroundColor: 'rgb(255, 74, 110)',
-
-              ...(window.width > 980
-                ? {
-                    width: '50%',
-                    float: 'right',
-                    height: '660px',
-                    padding: '110px',
-                  }
-                : {
-                    display: 'block',
-                    width: '100%',
-                    padding: '90px 40px',
-                    height: '550px',
-                  }),
-            }}
-          >
-            <div>
-              <div
-                style={{
-                  ...(window.width > 980
-                    ? { marginLeft: '40px' }
-                    : { marginLeft: '0' }),
-                }}
-              >
-                <div
+            <div
+              style={{
+                ...(window.width > 980
+                  ? {
+                      width: '50%',
+                      float: 'left',
+                    }
+                  : {
+                      display: 'block',
+                      width: '100%',
+                      paddingBottom: '70px',
+                    }),
+              }}
+            >
+              <form>
+                <h1
                   style={{
-                    display: 'block',
+                    fontWeight: '200',
 
+                    lineHeight: '1.5em',
+                    letterSpacing: '3px',
                     ...(window.width > 980
                       ? {
-                          margin: '30px auto',
-                          width: '340px',
+                          fontSize: '50px',
                         }
                       : {
-                          margin: '0 18px',
+                          fontSize: '28px',
                         }),
                   }}
                 >
-                  <i className="fa fa-map-marker" style={style.icons} />
-                  <p style={style.contactTitles}>Address </p>
-                  <p style={style.contactText} id="address">
-                    Porcelænshaven 26, 2000
-                  </p>
-                  <p style={style.contactText}>Frederiksberg 2000</p>
-                </div>
-                <div
+                  Contact
+                </h1>
+                <label
+                  for="send-address"
                   style={{
+                    textAlign: 'center',
                     display: 'block',
-
-                    ...(window.width > 980
-                      ? {
-                          width: '340px',
-                          margin: '50px auto',
-                        }
-                      : { margin: '50px 18px' }),
+                    marginTop: '25px',
+                    fontWeight: '100',
+                    color: '#333',
                   }}
                 >
-                  <i className="fa fa-phone" style={style.icons} />
-                  <p style={style.contactTitles}>Telephon</p>
-                  <p style={style.contactText}> +45 89 80 12 80</p>
-                </div>
-                <div
+                  Your email please
+                </label>
+                <input
+                  required
+                  id="send-address"
+                  maxLength="256"
+                  name="send address"
+                  type="email"
                   style={{
                     display: 'block',
-
-                    ...(window.width > 980
-                      ? {
-                          margin: '50px auto',
-                          width: '340px',
-                        }
-                      : { margin: '50px 18px' }),
+                    margin: '10px auto',
+                    maxWidth: '300px',
+                    width: '300px',
+                    border: '1px solid rgba(255,74,110,.5)',
+                    borderRadius: '3px',
+                    padding: '10px 15px',
+                    outline: '#000',
+                    fontSize: 'inherit',
+                  }}
+                />
+                <label
+                  for="message"
+                  style={{
+                    fontWeight: '100',
+                    textAlign: 'center',
+                    display: 'block',
+                    marginTop: '25px',
+                    color: '#333',
                   }}
                 >
-                  <i className="fa fa-envelope" style={style.icons} />
-                  <p style={style.contactTitles}>E-mail</p>
-                  <p style={style.contactText} id="support">
-                    info@billetfix.dk
-                  </p>
+                  Message
+                </label>
+                <textarea
+                  required
+                  id="message"
+                  rows="10"
+                  style={{
+                    outline: 'none',
+                    width: '90%',
+                    maxWidth: ' 400px',
+                    fontFamily: 'inherit',
+                    fontSize: 'inherit',
+                    margin: '10px',
+                    lineHeight: 'inherit',
+                    border: '1px solid rgba(255,74,110,.5)',
+                    borderRadius: '3px',
+                    padding: '.5em .75em',
+                  }}
+                />
+                <button
+                  type="submit"
+                  style={{
+                    letterSpacing: '2px',
+                    borderColor: '#ff4a6e',
+                    display: 'block',
+                    background: '#ff4a6e',
+                    color: '#fff',
+                    borderRadius: '2px',
+                    margin: ' auto',
+                    cursor: 'pointer',
+                    padding: '10px 25px',
+                    textAlign: 'center',
+                    fontWeight: '300',
+                    fontSize: '13px',
+                    border: '1px solid #ff4a6e',
+
+                    transition: 'all .15s',
+                  }}
+                >
+                  SEND!
+                </button>
+              </form>
+            </div>
+            <div
+              style={{
+                backgroundColor: 'rgb(255, 74, 110)',
+
+                ...(window.width > 980
+                  ? {
+                      width: '50%',
+                      float: 'right',
+                      height: '660px',
+                      padding: '110px',
+                    }
+                  : {
+                      display: 'block',
+                      width: '100%',
+                      padding: '90px 40px',
+                      height: '550px',
+                    }),
+              }}
+            >
+              <div>
+                <div
+                  style={{
+                    ...(window.width > 980
+                      ? { marginLeft: '40px' }
+                      : { marginLeft: '0' }),
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'block',
+
+                      ...(window.width > 980
+                        ? {
+                            margin: '30px auto',
+                            width: '340px',
+                          }
+                        : {
+                            margin: '0 18px',
+                          }),
+                    }}
+                  >
+                    <i className="fa fa-map-marker" style={style.icons} />
+                    <p style={style.contactTitles}>Address </p>
+                    <p style={style.contactText} id="address">
+                      Porcelænshaven 26, 2000
+                    </p>
+                    <p style={style.contactText}>Frederiksberg 2000</p>
+                  </div>
+                  <div
+                    style={{
+                      display: 'block',
+
+                      ...(window.width > 980
+                        ? {
+                            width: '340px',
+                            margin: '50px auto',
+                          }
+                        : { margin: '50px 18px' }),
+                    }}
+                  >
+                    <i className="fa fa-phone" style={style.icons} />
+                    <p style={style.contactTitles}>Telephon</p>
+                    <p style={style.contactText}> +45 89 80 12 80</p>
+                  </div>
+                  <div
+                    style={{
+                      display: 'block',
+
+                      ...(window.width > 980
+                        ? {
+                            margin: '50px auto',
+                            width: '340px',
+                          }
+                        : { margin: '50px 18px' }),
+                    }}
+                  >
+                    <i className="fa fa-envelope" style={style.icons} />
+                    <p style={style.contactTitles}>E-mail</p>
+                    <p style={style.contactText} id="support">
+                      info@billetfix.dk
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-      )}
+          </section>
+        )
+      }}
     </WindowHelper>
   </Layout>
 )
--- src/components/ticket.js
+++ src/components/ticket.js
@@ -8,121 +8,128 @@ import Trash from '../img/trash.png'
 import { WindowHelper } from '../components/helpers'
 import AddButton from '../img/plus-button.png'
 
-let styles = {
-  label: {
-    display: 'block',
-    fontSize: '1.2em',
-    verticalAlign: 'bottom',
-    margin: '20px 0',
-    position: 'relative',
-    fontWeight: '100',
-  },
-  input: {
-    display: 'block',
-    margin: '10px 0',
-
-    fontSize: 'medium',
-    padding: '11px 10px',
-    fontWeight: '100',
-    border: '1px solid rgba(255, 74, 110, 0.5)',
-    borderRadius: '3px',
-    letterLetterspacing: '1px',
-    width: '300px',
-  },
-}
-
 const NewTicket = ({ ticket, i, onRemove }) => (
   <WindowHelper>
-    {window => (
-      <div
-        style={{
-          border: ' 1px solid #ff4a6e',
-          borderRadius: '3px',
-          margin: '0 20px 20px 0',
-          padding: '20px',
+    {window => {
+      let styles = {
+        label: {
+          display: 'block',
+          fontSize: '1.2em',
+          verticalAlign: 'bottom',
+          margin: '20px 0',
+          position: 'relative',
+          fontWeight: '100',
+        },
+        input: {
+          display: 'block',
+          margin: '10px 0',
 
-          height: '368px',
-          overflow: 'hidden',
-          display: 'flex',
-          ...(window.width > 980
+          fontSize: 'medium',
+          padding: '11px 10px',
+          fontWeight: '100',
+          border: '1px solid rgba(255, 74, 110, 0.5)',
+          borderRadius: '3px',
+          letterLetterspacing: '1px',
+          ...(window.width > 450
             ? {
-                width: '342px',
+                width: '300px',
               }
             : {
-                width: '315px',
+                width: '275px',
               }),
-        }}
-      >
-        <div className="tickets">
-          <div className="ticket dynamic-form row1">
-            <label style={styles.label} className="type-label">
-              Ticket Name
-              <input
-                style={styles.input}
-                required=""
-                type="text"
-                id="id_form-0-title"
-                name="form-0-title"
-                placeholder="Entry / Standing / Gold / VIP"
-                maxLength="32"
-              />
-            </label>
+        },
+      }
+      return (
+        <div
+          style={{
+            border: ' 1px solid #ff4a6e',
+            borderRadius: '3px',
+            margin: '0 20px 20px 0',
+            padding: '7px',
 
-            <label
-              style={styles.label}
-              className="type-label"
-              htmlFor="id_price"
-            >
-              Price
-              <input
-                style={styles.input}
-                required=""
-                type="number"
-                id="id_price"
-                name="form-0-price"
-                min="0"
-                placeholder="kr - or '0' for FREE"
-                step="1"
-              />
-            </label>
+            height: '368px',
+            overflow: 'hidden',
+            display: 'flex',
+            ...(window.width > 980
+              ? {
+                  width: '342px',
+                }
+              : {
+                  width: '315px',
+                }),
+          }}
+        >
+          <div className="tickets">
+            <div className="ticket dynamic-form row1">
+              <label style={styles.label} className="type-label">
+                Ticket Name
+                <input
+                  style={styles.input}
+                  required=""
+                  type="text"
+                  id="id_form-0-title"
+                  name="form-0-title"
+                  placeholder="Entry / Standing / Gold / VIP"
+                  maxLength="32"
+                />
+              </label>
 
-            <label
-              style={styles.label}
-              className="type-label"
-              htmlFor="id_amount"
-            >
-              Number of tickets
-              <input
-                style={styles.input}
-                required=""
-                type="number"
-                min="1"
-                id="id_amount"
-                name="form-0-amount"
-                placeholder="Pcs"
-              />
-            </label>
-            <a
-              id="delete-ticket"
-              className="delete-row"
-              href="javascript:void(0)"
-              onClick={() => onRemove(i)}
-            >
-              <img
-                style={{
-                  height: ' 30px',
-                  width: 'auto',
-                  float: 'right',
-                }}
-                className="trash"
-                src={Trash}
-                alt="trash"
-              />
-            </a>
+              <label
+                style={styles.label}
+                className="type-label"
+                htmlFor="id_price"
+              >
+                Price
+                <input
+                  style={styles.input}
+                  required=""
+                  type="number"
+                  id="id_price"
+                  name="form-0-price"
+                  min="0"
+                  placeholder="kr - or '0' for FREE"
+                  step="1"
+                />
+              </label>
+
+              <label
+                style={styles.label}
+                className="type-label"
+                htmlFor="id_amount"
+              >
+                Number of tickets
+                <input
+                  style={styles.input}
+                  required=""
+                  type="number"
+                  min="1"
+                  id="id_amount"
+                  name="form-0-amount"
+                  placeholder="Pcs"
+                />
+              </label>
+              <a
+                id="delete-ticket"
+                className="delete-row"
+                href="javascript:void(0)"
+                onClick={() => onRemove(i)}
+              >
+                <img
+                  style={{
+                    height: ' 30px',
+                    width: 'auto',
+                    float: 'right',
+                  }}
+                  className="trash"
+                  src={Trash}
+                  alt="trash"
+                />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )
+    }}
   </WindowHelper>
 )
 
